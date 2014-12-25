508930fca80cbc32eef9392b083d20f20f665dd2
Edward Ross <edward@skeptric.com>
2014-12-26 10:42:54 +1100

    Restructured removing contact page

--- site.hs
+++ site.hs
@@ -16,7 +16,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
