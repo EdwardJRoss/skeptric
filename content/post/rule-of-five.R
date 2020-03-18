2e172da817bb3bbf6ae1eea2cc58037c7421c012
Edward Ross <edward@skeptric.com>
2020-03-18 22:33:07 +1100

    Finish rule of 5 post

--- content/post/rule-of-five.R
+++ content/post/rule-of-five.R
@@ -1,31 +1,32 @@
 # Exact calculation
-ci_median_extreme <- function(num_samples, nth_largest=0, nth_smallest=0) {
+ci_quantile_extreme <- function(num_samples, nth_smallest=1, nth_largest=1, quantile=0.5) {
     stopifnot(nth_largest + nth_smallest < num_samples)
-    # Number of ways all but the nth largest values could be bigger than median
-    freq_larger <- sum(choose(num_samples, seq(0, nth_largest)))
-    # Number of ways all but the nth smallest values could be bigger than median
-    freq_smaller <- sum(choose(num_samples, seq(0, nth_smallest)))
-    1 - (freq_larger + freq_smaller) / 2^num_samples
+    stopifnot((0 < quantile) && (quantile < 1))
+    # Number of ways all but the nth largest values could be bigger than quantile
+    freq_larger <- sum(choose(num_samples, seq(0, nth_largest - 1)))
+    # Number of ways all but the nth smallest values could be bigger than quantile
+    freq_smaller <- sum(choose(num_samples, seq(0, nth_smallest - 1)))
+    1 - (freq_larger * quantile ^ num_samples + freq_smaller * (1 - quantile) ^num_samples)
 }
 
-# Test calculation with random normal sample
-sample_median_extreme <- function(sample_size, nth, num_sample=10000) {
-    distribution <- rnorm
-    distribution_median <- 0
+# Test calculation with random sample
+sample_quantile_extreme <- function(sample_size, nth_smallest, nth_largest, num_sample=10000, quantile=0.5) {
+    distribution <- runif
+    distribution_median <- quantile
 
     samples <- distribution(sample_size * num_sample)
     dim(samples) <- c(sample_size, num_sample)
 
     sorted_samples <- apply(samples, 2, sort)
-    mean((sorted_samples[1+nth,] <= distribution_median) &
-         (sorted_samples[sample_size-nth,] >= distribution_median))
+    mean((sorted_samples[nth_smallest,] <= distribution_median) &
+         (sorted_samples[sample_size+1-nth_largest,] >= distribution_median))
 }
 
 
 # Calculate the number of samples at nth largest to reach at least ci
 median_extreme_samples_at_ci <- function(nth, ci=0.9) {
     sample_size <- 2*nth + 1
-    while(ci_median_extreme(sample_size, nth, nth) < ci) {
+    while(ci_quantile_extreme(sample_size, nth, nth) < ci) {
         sample_size <- sample_size + 1
     }
     sample_size
@@ -33,21 +34,39 @@ median_extreme_samples_at_ci <- function(nth, ci=0.9) {
 
 # Generate a table of confidences
 ci_median_extreme_table <- function(n_max=10, ci=0.9) {
-    nth_largest <- seq(0, n_max)
+    nth_largest <- seq(1, n_max)
     sample_size <- vapply(nth_largest,
                           function(n) median_extreme_samples_at_ci(n, ci),
                           double(1))
-    confidence_interval <- mapply(ci_median_extreme, sample_size, nth_largest, nth_largest)
+    confidence_interval <- mapply(ci_quantile_extreme, sample_size, nth_largest, nth_largest)
 
-    data.frame(nth_largest, sample_size, confidence_interval)
+    data.frame(nth_largest, sample_size, confidence_interval, ci=ci)
 }
 
 # Power depends a lot on the distribution
 
-ci_median_extreme_table(10, 0.9)
+df <- rbind(
+ci_median_extreme_table(20, 0.75),
+ci_median_extreme_table(20, 0.9),
+ci_median_extreme_table(20, 0.95),
+ci_median_extreme_table(20, 0.99)
+)
 
-ci_median_extreme_table(10, 0.75)
+df$confidence_interval = paste0(as.character(df$ci * 100), '%')
 
-ci_median_extreme_table(10, 0.95)
+library(ggplot2)
 
-ci_median_extreme_table(10, 0.99)
+ggplot(df, mapping=aes(nth_largest, sample_size, color=confidence_interval, group=confidence_interval)) +
+    geom_line() +
+    scale_x_continuous(minor_breaks=seq(1, 20)) +
+    scale_y_continuous(minor_breaks=seq(0, 100, by=5)) +
+    labs(x="nth largest/smallest items", y="Sample Size", color="Confidence Interval",
+         title="Median confidence interval between nth extreme items")
+
+ggsave('rule-of-n.png')
+
+# Estimate first quartile?
+# For max power do we try to max n_largest + n_smallest?
+sample_quantile_extreme(20, 1, 11, 10000, 0.25)
+
+sample_quantile_extreme(20, 2, 0, 10000, 0.25)
